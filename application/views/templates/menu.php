4c1c53a0f847a7583e621c186d44033c95e550ce
albuquerque.r.lucas <albuquerque.r.lucas@gmail.com>
2016-05-20 14:57:18 -0300

    Alteração no meu

--- application/views/templates/menu.php
+++ application/views/templates/menu.php
@@ -9,6 +9,7 @@
                 <li><a href="<?php echo base_url('Geral/produtos'); ?>">Produtos</a></li>
                 <li><a href="<?php echo base_url('Geral/encomendas'); ?>">Encomendas</a></li>
                 <li><a href="<?php echo base_url('Geral/contato'); ?>">Contato</a></li>
+                <li><a href="<?php echo base_url('Geral/login'); ?>" >Acesso</a></li>
             </ul>
         </div>
     </div>
